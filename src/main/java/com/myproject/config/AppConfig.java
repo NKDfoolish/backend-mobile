21158f9c8cd3e78c13ee92432cf69f92439bfe9d
NKDfoolish <nguyenkhanhduy.dilinh@gmail.com>
2025-03-23 20:50:37 +0700

    spring security config

--- src/main/java/com/myproject/config/AppConfig.java
+++ src/main/java/com/myproject/config/AppConfig.java
@@ -1,32 +1,47 @@
 package com.myproject.config;
 
+import com.myproject.service.UserServiceDetail;
 import com.sendgrid.SendGrid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
     @Value("${spring.sendgrid.apiKey}")
     private String sendGridApiKey;
+
+    private final CustomizerRequestFilter requestFilter;
+    private final UserServiceDetail userServiceDetail;
 
     // Init spring web security
     // Config spring web configurer
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll()
+                .authorizeHttpRequests(request -> request.requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS));
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -35,6 +50,35 @@ public class AppConfig {
         return webSecurity -> webSecurity
                 .ignoring()
                 .requestMatchers("/actuator/**", "/v3/**", "/webjars/**", "/swagger-ui*/*swagger-initializer.js", "/swagger-ui*/**", "/favicon.ico");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed request headers
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userServiceDetail.userDetailsService());
+
+        return provider;
     }
 
     //init bean for password encoder
---
+++ src/main/java/com/myproject/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package com.myproject.controller;
+
+import com.myproject.controller.request.SignInRequest;
+import com.myproject.controller.response.TokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Slf4j(topic = "AUTHENTICATION_CONTROLLER")
+@Tag(name = "Authentication Controller", description = "APIs for authentication")
+public class AuthenticationController {
+
+    @Operation(summary = "Access token", description = "Get access token and refresh token by username and password")
+    @PostMapping("/access-token")
+    public TokenResponse getAccessToken(@RequestBody SignInRequest request) {
+        log.info("Access token request");
+
+        return TokenResponse.builder()
+                .accessToken("DUMMY_ACCESS_TOKEN")
+                .refreshToken("DUMMY_REFRESH_TOKEN")
+                .build();
+    }
+
+    @Operation(summary = "Refresh token", description = "Get new access token by refresh token")
+    @PostMapping("/refresh-token")
+    public TokenResponse getRefreshToken(@RequestBody String refreshToken) {
+        log.info("Refresh token request");
+
+        return TokenResponse.builder()
+                .accessToken("DUMMY_NEW_ACCESS_TOKEN")
+                .refreshToken("DUMMY_REFRESH_TOKEN")
+                .build();
+    }
+}
